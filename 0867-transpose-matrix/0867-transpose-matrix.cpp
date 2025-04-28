df164685eed5b117fa9acf8d76fa9d9cd416011f
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2025-04-28 19:27:42 +0530

    Time: 0 ms (100%), Space: 14.9 MB (92.36%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int rowSize = matrix.size();
-        int colSize = matrix[0].size();
+        int rowofOriginalMatrix = matrix.size();
+        int colOfOriginalMatrix = matrix[0].size();
 
-        // original matrix = rowSize * colSize;
-        // transposed matrix = colSize * rowSize;
-        vector<vector<int>> transposedMatrix(colSize, vector<int>(rowSize));
+        // original matrix = rowofOriginalMatrix * colOfOriginalMatrix;
+        // transposed matrix = colOfOriginalMatrix * rowofOriginalMatrix;
 
-        for(int row=0; row<rowSize; row++)
+        int rowsOftransposedMatrix = colOfOriginalMatrix;
+        int colOftransposedMatrix = rowofOriginalMatrix;
+        vector<vector<int>> transposedMatrix(rowsOftransposedMatrix, vector<int>(colOftransposedMatrix, 0));
+
+        for(int row=0; row<rowsOftransposedMatrix; row++)
         {
-            for(int col=0; col<colSize; col++)
+            for(int col=0; col<colOftransposedMatrix; col++)
             {
-                transposedMatrix[col][row] = matrix[row][col];
+                transposedMatrix[row][col] = matrix[col][row];
             }
         }
         return transposedMatrix;
