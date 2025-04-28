3a659fdf2bea2c966a48b20133b9be437d41de10
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2025-04-28 19:20:26 +0530

    Time: 0 ms (100%), Space: 15.2 MB (6.54%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -3,6 +3,9 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int rowSize = matrix.size();
         int colSize = matrix[0].size();
+
+        // original matrix = rowSize * colSize;
+        // transposed matrix = colSize * rowSize;
         vector<vector<int>> transposedMatrix(colSize, vector<int>(rowSize));
 
         for(int row=0; row<rowSize; row++)
