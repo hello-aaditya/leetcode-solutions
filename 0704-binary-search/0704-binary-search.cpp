c64b2458b7fa10488212c701b7039be3ec062d3c
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2025-04-29 01:11:21 +0530

    Time: 0 ms (100%), Space: 31.2 MB (80.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int size = nums.size();
+        int size=nums.size();
 
-        if(size == 1 && nums[0]==target) return 0;
-        else if(size == 1 && nums[0]!=target) return -1;
+        // initialize two variable: 1. left pointer 2. right pointer
+        int leftPtr = 0, rightPtr = size-1;
 
-        int leftPtr = 0;
-        int rightPtr = size-1;
-
+        // ensure to check in valid range
         while(leftPtr<=rightPtr)
         {
             int mid = leftPtr+(rightPtr-leftPtr)/2;
+
+            // if target is found in the mid index then return the mid index
             if(nums[mid] == target)
             {
                 return mid;
             }
-
-            else if(nums[mid] < target)
-            {
-                leftPtr = mid+1;
-            }
-
+            // if the element in mid index > target then update rightPtr
             else if(nums[mid] > target)
             {
                 rightPtr = mid-1;
             }
+            // if the element in mid index < target then update leftPtr
+            else
+            {
+                leftPtr = mid+1;
+            }
         }
         return -1;
-        
     }
 };
