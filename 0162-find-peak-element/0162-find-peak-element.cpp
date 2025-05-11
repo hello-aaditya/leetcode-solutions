7045b472efd7b5104b1c1b3ee019dd8fc8690167
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2025-05-11 23:08:58 +0530

    Time: 0 ms (100%), Space: 12.6 MB (32.23%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int size = nums.size();
+
+        // edge-case-1: where size = 1
+        if(size == 1)   return 0;
+
+        // Edge Case 2: Check if peak exists at the boundaries (0 or n-1)
+        // Because nums[-1] and nums[n] are considered -∞
+
+        if(nums[0] > nums[1])   return 0;
+        if(nums[size-1] > nums[size-2]) return size-1;
+
+        // Main Logic: Binary Search in O(log n) time
+        int start = 0, end = size-1;
+        while(start<=end)
+        {
+            // Bitwise trick to compute mid safely without overflow or division
+            int mid = (start & end) + ((start ^ end) >> 1);
+            if( (mid-1)!=-1 && (mid+1)!=size && nums[mid]>nums[mid+1] && nums[mid] > nums[mid-1])
+            {
+                return mid;
+            }
+            else if(nums[mid] < nums[mid+1])
+            {
+                start = mid+1;
+            }
+            else if(nums[mid] < nums[mid-1])
+            {
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+};
