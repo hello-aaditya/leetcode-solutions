f78dade45b58cbd7f6b7769623d28ff6a6f73f0a
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-01-15 16:04:04 +0530

    Time: 1 ms (99.7%), Space: 94.4 MB (56.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,29 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int size = prices.length;
+        // KADANE'S ALGORITHM
+        int minPrice = prices[0];
+        int maxProfit = 0;
 
-        int[] left = new int[size];
-        int[] right = new int[size];
-
-        left[0] = prices[0];
-        right[size-1] = prices[size-1];
-
-        // CALCULATE MIN PRICE AND STORE IN 'left'
-        for (int i=1; i<size; i++) {
-            left[i] = Math.min(left[i-1], prices[i]);
+        for (int i=0; i<prices.length; i++) {
+            int currentProfit = prices[i] - minPrice;
+            maxProfit = Math.max(currentProfit, maxProfit);
+            minPrice = Math.min(minPrice, prices[i]);
         }
 
-        // CALCULATE MAX PRICE AND STORE IN 'right'
-        for (int i=size-2; i>=0; i--) {
-            right[i] = Math.max(right[i+1], prices[i]);
-        }
-
-        int profit = 0;
-
-        // COUNT MAX PROFIT BY SUBTRACTING MAX PRICE - MIN PRICE AND CONSIDER THE MAX PROFIT
-        for (int i=0; i<size; i++) {
-            profit = Math.max(profit, right[i] - left[i]);
-        }
-        return profit;
+        return maxProfit;
     }
 }
