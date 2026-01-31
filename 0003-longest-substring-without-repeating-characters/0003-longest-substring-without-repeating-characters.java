a577f2bab36892bc177d50078e559704565e889d
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-01-31 11:01:36 +0530

    Time: 5 ms (85.54%), Space: 45.5 MB (74.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,30 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int size = s.length();
+        int size = s.length(); // SIZE OF THE STRING s
         int start = 0;
-        int longestSubstring = 0;
+        int maxLength = 0; // TO STORE MAXIMUM-LENGTH OF LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS
 
-        ArrayList<Character> list = new ArrayList<>();
+        // HASMAP TO STORE THE EVERY CHARACTERS AND ITS CURRENT POSITION
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        for (int end=0; end<size; end++) {
-            char currentChar = s.charAt(end);
-            
+        for (int end =0; end<size; end++) {
+            char currentChar = s.charAt(end); // CURRENT CHARACTER
 
-            if (list.contains(currentChar)) {
-                while (s.charAt(start) != currentChar) {
-                    char firstChar = s.charAt(start);
-                    list.remove(0);
-                    start++;
-                }
-                if (list.get(0) == currentChar) {
-                    list.remove(0);
-                }
-                start++;
+            // IF currentChar FOUND INSIDE MAP THEN UPDATE start TO ITS CURRENT OCCURRENCE POSITION
+            if(map.containsKey(currentChar)) {
+                start = Math.max(start, map.get(currentChar) + 1);
             }
 
-            list.add(currentChar);
-            longestSubstring = Math.max(longestSubstring, list.size());
+            // INSERT currentChar ALONG WITH ITS POSITION TO THE MAP
+            map.put(currentChar, end);
+
+            int windowSize = end - start + 1; 
+            maxLength = Math.max(maxLength, windowSize);
         }
-        return longestSubstring;
+        return maxLength;
     }
 }
