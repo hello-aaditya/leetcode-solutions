e8c55556f75cc5913c202131e63effb9c5bc05d8
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-02-07 10:13:31 +0530

    Time: 0 ms (100%), Space: 47.6 MB (18.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,19 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        /* 
-            Approach: find the sum of all natural number till n+1 and minus the total 
-            sum to each of the elements present inside the array
-         */
-
-         // FIND THE TOTAL SUM TILL size + 1
         int size = nums.length;
 
-        int totalSum = size * (size + 1) / 2;
+        // SUM OF n NATURAL NUMBER = n * (n+1) / 2
+
+        int sum = size * (size + 1) / 2;
 
         for (int i=0; i<size; i++) {
-            totalSum -= nums[i];
+            sum -= nums[i];
         }
-        return totalSum;
-        
+        return sum;
     }
 }
