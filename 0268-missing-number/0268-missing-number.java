0b613d8cd259c2d054fc100939bb2cdec0c7d2e6
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-03-12 16:47:29 +0530

    Time: 1 ms (28.85%), Space: 47.2 MB (81.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int size = nums.length;
-        int start = 0;
-        while (start < size) {
-            int correctIndex = nums[start];
-            if ((correctIndex < size) && (nums[correctIndex] != nums[start])) {
+        int i=0;
+
+        while (i < size) {
+            int correctIndex = nums[i];
+            if ((correctIndex != size) && (nums[i] != nums[correctIndex])) {
                 int temp = nums[correctIndex];
-                nums[correctIndex] = nums[start];
-                nums[start] = temp;
+                nums[correctIndex] = nums[i];
+                nums[i] = temp;
             } else {
-                start++;
+                i++;
             }
         }
 
-        for (int i=0; i<size; i++) {
-            if (i != nums[i]) {
-                return i;
+        for (int j=0; j<size; j++) {
+            if (j != nums[j]) {
+                return j;
             }
         }
         return size;
