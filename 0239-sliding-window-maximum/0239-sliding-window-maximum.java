d9285de58262a9172d9d9106d5df607c4a579c03
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-04-24 18:12:33 +0530

    Time: 29 ms (84.7%), Space: 147.9 MB (40.32%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,45 +1,35 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int size = nums.length;
-        int start = 0;
 
-        // ARRAYDEQUE CONTAINS THE MAX VALUES FROM nums
-        ArrayDeque<Integer> deq = new ArrayDeque<>();
-        deq.add(Integer.MIN_VALUE); // INITIALLY deq CONTAINS A SMALLEST VALUE IN JAVA
+        int start = 0, end = 0;
+        int[] maxArr = new int[size-k+1];
+        int idx = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        int[] max = new int[size - k + 1]; // STORES THE MAXIMUM AMONGST k ELEMENT FROM nums
-        int index = 0; // IS USED TO STORE VALUES INSIDE max
+        while (end < size) {
+            int windowSize = end-start+1;
+            int current = nums[end];
 
-        for (int end=0; end<size; end++) {
-            int windowSize = end - start + 1; // FIND windowSize
-            int dqSize = deq.size(); // FIND deq SIZE
-
-            // IF deq IS NOT EMPTY AND nums[end] > THE LAST VALUE OF deq THEN DELETE IT FROM THE deq
-            while ( (!deq.isEmpty()) && (nums[end] > deq.peekLast()) ) { 
-                deq.pollLast();
+            while ((!dq.isEmpty()) && (current > dq.peekLast())) {
+                dq.pollLast();
             }
 
-            // ADD ELEMENT (nums[end]) INSIDE deq
-            deq.add(nums[end]);
+            dq.offerLast(current);
 
-            // IF nums[end] < THE LAST ELEMENT OF deq THEN ADD IT INSIDE deq BECAUSE MAY BE IT WILL BE A MAXIMUM FOR UPCOMING WINDOW
-            if (nums[end] < deq.peekLast()) {
-                deq.add(nums[end]);
+            if (windowSize < k) {
+                end++;
             }
-
-            // IF WE HIT THE windowSize THEN
-            if (windowSize == k) {
-                max[index++] = deq.peekFirst(); // ADD THE 1st ELEMENT OF deq TO THE index-th POSITION OF max
-                // IT'S TIME TO SLIDE THE WINDOW BUT BEFORE SLIDING CHECK WHETHER (nums[start] == FIRST ELEMENT OF deq), IF SO THEN
-                // REMOVE THE ELEMENT FROM deq
-                if (nums[start] == deq.peekFirst()) {
-                    deq.pollFirst();
+            else if (windowSize == k) {
+                maxArr[idx++] = dq.peekFirst();
+                
+                if (nums[start] == dq.peekFirst()) {
+                    dq.pollFirst();
                 }
-                // AT THE END SLIDE THE WINDOW
                 start++;
+                end++;
             }
-            
         }
-        return max;
+        return maxArr;
     }
 }
