6bfd825a0d7d98bb47fc14899f9421faf19342d8
Aaditya Kumar <174272227+hello-aaditya@users.noreply.github.com>
2026-05-14 08:51:41 +0530

    Time: 9 ms (42.57%), Space: 41.9 MB (84.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,11 @@
 class Solution {
+    static int fibonacci = 0;
     public int fib(int n) {
         // BASE CONDITION
-        if (n < 2) {
+        if ( (n == 0) || (n == 1) ) {
             return n;
         }
         
-        return fib(n-1) + fib(n-2);
+        return (fib(n-1) + fib(n-2));
     }
 }
